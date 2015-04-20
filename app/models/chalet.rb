9a14ef3c5eb546f613761f9ce01fdf58670faf50
Willem <willem.browne@gmail.com>
2015-04-20 14:47:01 +0200

    added Chalet model and associated AR migration

---
+++ app/models/chalet.rb
@@ -0,0 +1,9 @@
+class Chalet < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, presence: true, uniqueness: true
+  validates :city, presence: true
+  validates :daily_price, presence: true, numericality: true
+  validates :max_nb_of_guests, presence: true, numericality: true
+  validates :availability, presence: true
+end
