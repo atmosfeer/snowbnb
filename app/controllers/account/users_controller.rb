8386848b9409ade9053bdc2f607d302655bd3311
marina starkova <marina.starkova@gmail.com>
2015-04-20 19:28:41 +0200

    controllers done but there might be something wrong with them

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,6 +1,19 @@
 module Account
   class UsersController < BaseController
+      def edit
+      @user = current_user
+    end
 
+    def update
+      @user = current_user
+      @user.update(user_params)
+    end
   end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password, :picture)
+    end
 
 end
