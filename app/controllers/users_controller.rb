8d6105fb04ddac00e9ff0b23900bc47573478900
marina starkova <marina.starkova@gmail.com>
2015-04-21 14:16:43 +0200

    the signup fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+
+
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password, :picture)
   end
 end
