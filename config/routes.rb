37684587c45d08ea3d138fbf255c38c5f9f8f60c
Nicolas Feer <atmosfeer@gmail.com>
2015-04-21 11:37:00 +0200

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
 
+  root to: "home#index"
+
   devise_for :users
 
-  resources :chalets, only: [:index, :show]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:show]
 
-  resources :chalets, only: :show do
+  resources :chalets, only: [:index, :show] do
     resources :bookings, only: [:new, :create, :edit, :update, :destroy]
   end
 
   namespace :account do
-    resources :users, only: [:edit, :update, :show]
+    resource :user, only: [:show]
     resources :chalets, only: [:new, :create, :edit, :update, :destroy, :index]
     resources :bookings, only: :index
   end
 
-  root to: "home#index"
 
-  resources :chalets, only: [:index]
 end
