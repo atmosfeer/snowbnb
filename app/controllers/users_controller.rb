d8f9c9fc17406f3efa9dd6ab2d83042d8f03aff3
marina starkova <marina.starkova@gmail.com>
2015-04-22 17:35:07 +0200

    default upic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user.picture = default_picture
+
 
   end
 
@@ -14,11 +14,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password, :picture)
   end
 
-  def default_picture
-  if @user.picture.empty?
-    image_tag "default_picture.jpg"
-  else
-    @user.picture
-  end
-end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :chalets
   has_many :bookings
 
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ':style/default-picture.png'
 
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   validates :email, uniqueness: true, presence: true
