e1362aa6fb38e2cfabffd6611880ada3c34bb1b6
marina starkova <marina.starkova@gmail.com>
2015-04-22 16:06:34 +0200

    links to chalets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user.picture = default_picture
+
   end
 
   private
@@ -11,4 +13,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password, :picture)
   end
+
+  def default_picture
+  if @user.picture.empty?
+    image_tag "default_picture.jpg"
+  else
+    @user.picture
+  end
+end
 end
