607bab58207dfcb387010548a19f6343882fe1c2
marina starkova <marina.starkova@gmail.com>
2015-04-22 11:26:26 +0200

    pictures for chalets form updated

--- app/models/chalet_picture.rb
+++ app/models/chalet_picture.rb
@@ -3,4 +3,5 @@ class ChaletPicture < ActiveRecord::Base
 
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :picture, less_than: 5.megabytes
 end
