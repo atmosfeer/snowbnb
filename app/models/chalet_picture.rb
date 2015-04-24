f2e14b61e913f2c9d78a8cffa5ee315aa13cf208
marina starkova <marina.starkova@gmail.com>
2015-04-24 15:30:30 +0200

    minor corrections

--- app/models/chalet_picture.rb
+++ app/models/chalet_picture.rb
@@ -2,10 +2,10 @@ class ChaletPicture < ActiveRecord::Base
   belongs_to :chalet
 
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>", cover: "1200x600" },
+    styles: { medium: "400x400>", thumb: "100x100>", cover: "1200x600" },
     convert_options: {
       thumb: "-background white -compose Copy -gravity center -extent 100x100",
-      medium: "-background white -compose Copy -gravity center -extent 300x300",
+      medium: "-background white -compose Copy -gravity center -extent 400x400",
       cover: "-background white -compose Copy -gravity center -extent 800x500",
     }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
