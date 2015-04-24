3cee2f805d5a6a69e8890404a6f57c39fa7ea2e0
Willem <willem.browne@gmail.com>
2015-04-24 14:51:06 +0200

    mutating home

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
   $(document).mousemove(function( event ) {
-    var height = (event.pageY / $(document).height());
-    var width = (event.pageX / $(document).width());
-    var t = Math.floor(100 * height);
-    var b = Math.floor(100 * width);
-    var style = 'linear-gradient(rgba(' + t + ',' + t + ',' + t + ',0.5),rgba('+ b + ',' + b + ',' + b + ',0.5))';
-    console.log(style);
+    var height = 1 - (event.pageY / $(document).height());
+    var width = 1 - (event.pageX / $(document).width());
+    var h = Math.floor(100 * height);
+    var w = Math.floor(100 * width);
+    var r = Math.floor(113 + 142 * height);
+    var g = Math.floor(193 + 62 * height);
+    var b = Math.floor(208 + 47 * height);
+    var style = 'linear-gradient(rgba(' + h + ',' + h + ',' + h + ',0.5),rgba('+ w + ',' + w + ',' + w + ',0.5))';
+    console.log(r);
     $('.fullscreen-film').css({ 'background': style });
-    var fill = $('img.svg');
-    console.log(fill);
+    $('.logo #Logo').attr('fill', 'rgba(' + r + ',' + g + ',' + b + ',1)');
   });
 });
